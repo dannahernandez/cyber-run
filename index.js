539b34224e1c4ae671561a65099878440538bbe1
dannahernandez <hernandezpinilla.danna@gmail.com>
2021-05-21 16:08:15 -0500

    Styles corrections and logic improvements

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
+var count = 1;
+var k = 1;
 
 function character(number, nameLabel, label, img, end) {
-
     var i = 0;
-
-    while(i <= end){
-        document.getElementById(label).value = parseInt(i/10000) + "mts";
-        //Hace que el caballo se vaya moviendo a la derecha
-        document.getElementById(img).style.paddingLeft = 100 + parseInt(i/100) + "px";
+    while (i <= end) {
+        document.getElementById(label).value = parseInt(i / 10000) + "mts";
+        //Makes the character moves to the right
+        document.getElementById(img).style.paddingLeft =
+            100 + parseInt(i / 100) + "px";
         i++;
         console.log(i);
     }
+
     characterName = document.getElementById(nameLabel).value;
-    alert("The winner is: " + number + " - " + characterName);
+    podium(number);
+    alert("The "+ place() + " place is: "+ characterName);
 }
 
 function execute() {
-    console.log("Holiwis")
     document.getElementById("character1-img").style.display = "inline";
     document.getElementById("character2-img").style.display = "inline";
     document.getElementById("character3-img").style.display = "inline";
+    document.getElementById("input1").style.display = "inline";
+    document.getElementById("input2").style.display = "inline";
+    document.getElementById("input3").style.display = "inline";
+    document.getElementById("window").style.display = "none";
     Concurrent.Thread.create(character, 1, "inputCharacter1", "input1", "character1-img", 100000);
     Concurrent.Thread.create(character, 2, "inputCharacter2", "input2", "character2-img", 100000);
     Concurrent.Thread.create(character, 3, "inputCharacter3", "input3", "character3-img", 100000);
 }
 
+function podium(characterNumber) {
+    if (count === 1) {
+        noneCharacter("character" + characterNumber + "-img", "character" + characterNumber + "-win","input"+characterNumber, count);
+        count++;
+    } else {
+        noneCharacter("character" + characterNumber + "-img","character" + characterNumber + "-lose","input"+characterNumber, count);
+    }
+}
+
+function noneCharacter(element, element2,element3, position) {
+    if (position === 1) {
+        document.getElementById(element).style.display = "none";
+        document.getElementById(element3).style.display = "none";
+        document.getElementById(element2).style.display = "inline";
+    } else {
+        document.getElementById(element).style.display = "none";
+        document.getElementById(element3).style.display = "none";
+        document.getElementById(element2).style.display = "inline";
+    }
+}
+
+var place = () => k++;
 
